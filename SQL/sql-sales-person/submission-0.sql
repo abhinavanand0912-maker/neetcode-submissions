da06a1206e53ba793367891598ac929db4eaef81
Abhinav anand <abhinavanand0912@gmail.com>
2026-07-08 11:53:35 +0530

    Add: sql-sales-person - submission-0

---
+++ SQL/sql-sales-person/submission-0.sql
@@ -0,0 +1,11 @@
+SELECT name
+FROM sales_person
+WHERE sales_id NOT IN (
+    SELECT sales_id
+    FROM orders
+    WHERE com_id = (
+        SELECT com_id
+        FROM company
+        WHERE name = 'CRIMSON'
+    )
+);
