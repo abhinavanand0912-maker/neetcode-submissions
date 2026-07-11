67a0d7f406aee4cc1682c9be112f09207cf6eb08
Abhinav anand <abhinavanand0912@gmail.com>
2026-07-11 19:39:43 +0530

    Add: sql-customers-bought-a-b-not-c - submission-0

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-0.sql
@@ -0,0 +1,10 @@
+SELECT *
+FROM customers
+WHERE customer_id IN
+(SELECT customer_id
+FROM orders
+GROUP BY customer_id
+HAVING COUNT(CASE WHEN product_name = 'A' THEN 1 END) > 0 AND
+COUNT(CASE WHEN product_name = 'B' THEN 1 END) > 0 AND 
+COUNT(CASE WHEN product_name = 'C' THEN 1 END) = 0)
+ORDER BY customer_name;
